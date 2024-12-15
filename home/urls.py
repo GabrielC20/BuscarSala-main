f4b0b4d3f97a44c1e40eb80a4d186e87338a07a5
gabrielc20 <gabrielquadros157@gmail.com>
2024-12-15 08:30:05 -0300

    url modificada para que as alterações da view funcionasse

--- home/urls.py
+++ home/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('menu/',views.home, name='home'),
     path('pesquisar_sala/',views.pesquisar_sala, name = 'pesquisar_sala'), 
     path('video_sala/<str:id_sala>/',views.video_sala, name = 'video_sala'),
-    path('pavilhao/<str:numero_pavilhao>/',views.pavilhao,name='pavilhao'),
+    path('pavilhao/<int:numero_pavilhao>/', views.pavilhao, name='pavilhao'),
     path('', views.index, name='index'),
     path('deslogar/',views.deslogar, name='deslogar')
 
